5d2d9498ea978e9752354105cf019f5c7e26902a
Damiani Eric <eric.damiani@solera.com>
2022-10-19 14:35:21 +0200

    Created a new test and implemented the addNote

--- src/test/java/com/persistnotes/persistnotes/NotesList.java
+++ src/test/java/com/persistnotes/persistnotes/NotesList.java
@@ -1,17 +1,26 @@
 package com.persistnotes.persistnotes;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
 
 public class NotesList {
 	
-	private static int id = 0;
-	private static ArrayList<Notes> notesList;
+	private int id = 0;
+	private ArrayList<Notes> notesList;
 
-	public NotesList() {
-		notesList = new ArrayList<Notes>();
+	public ArrayList<Notes> NotesList() {
+		return notesList = new ArrayList<Notes>();
 	}
 	
 	public int incrementId() {
 		return ++id;
+	}
+
+	
+	//("some description", "some text", "2022-10-24", "some link", "some mention", "low");
+	public Notes createNote(String description, String text,String estimatedDate, String link, String mention, String priority) {
+		Notes newNote = new Notes(incrementId(), description, text, LocalDate.now(), LocalDate.parse(estimatedDate)
+					, null, link, mention, priority);
+		return newNote;
 	}
 }
--- src/test/java/com/persistnotes/persistnotes/PersistnotesApplicationTests.java
+++ src/test/java/com/persistnotes/persistnotes/PersistnotesApplicationTests.java
@@ -1,10 +1,12 @@
 package com.persistnotes.persistnotes;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import java.text.SimpleDateFormat;
+//import java.text.SimpleDateFormat;
 import java.time.LocalDate;
-import java.util.Date;
+//import java.util.Date;
+import java.util.ArrayList;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -21,12 +23,23 @@ class PersistnotesApplicationTests {
 		assertEquals(expectedValue,result);
 	}
 
-	Notes expectedNotes = new Notes(1, "some description", "some text", LocalDate.now(), LocalDate.parse("2022-10-24")
-			, null, "some link", "some mention", "low");
+	
 	@Test
 	public void addNote_WhenCalled_ReturnsNotes() {
+		Notes expectedNotes = new Notes(1, "some description", "some text", LocalDate.now(), LocalDate.parse("2022-10-24")
+				, null, "some link", "some mention", "low");
 		NotesList notesList = new NotesList();
 		Notes result = notesList.createNote("some description", "some text", "2022-10-24", "some link", "some mention", "low");
-		assertEquals(expectedValue, result);
+		//assertEquals(expectedNotes, (result));
+        assertThat(expectedNotes)
+        .usingRecursiveComparison()
+        .isEqualTo(result);
+	}
+	int expectedSizeArray=1;
+	@Test
+	public void addNoteToList_WhenCalled_ReturnsTheListWithObejectAdded() {
+		NotesList notesList = new NotesList();
+		ArrayList<Notes> list = notesList.addNoteToList();
+		assertEquals(expectedSizeArray,list.size());
 	}
 }
