a71b9bde36bfda8b715a833c2a15c95dbaf31821
Campos Diego Mauricio <diegomauricio.campos@solera.com>
2022-10-19 11:17:32 +0200

    project base

---
+++ src/main/java/com/persistnotes/persistnotes/PersistnotesApplication.java
@@ -0,0 +1,13 @@
+package com.persistnotes.persistnotes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersistnotesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersistnotesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/persistnotes/persistnotes/PersistnotesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.persistnotes.persistnotes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersistnotesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
