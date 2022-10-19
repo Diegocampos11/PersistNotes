86684ffb31ba8c19540d8e075c21c4fa0057ba72
Damiani Eric <eric.damiani@solera.com>
2022-10-19 16:15:32 +0200

    Added controller updated

--- src/main/java/com/persistnotes/persistnotes/Controller.java
+++ src/main/java/com/persistnotes/persistnotes/Controller.java
@@ -2,17 +2,32 @@ package com.persistnotes.persistnotes;
 
 import java.util.ArrayList;
 
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.pera.rest.webservices.restufulwebservices.user.User;
+import com.pera.rest.webservices.restufulwebservices.user.UserNotFoundException;
 
 @RestController
 public class Controller {
 	
-	@RequestMapping("/test")
+	@GetMapping("/notes")
 	public ArrayList<Notes> retrieveAllCourses(){
 		NotesList notesList = new NotesList();
 		Notes note = notesList.createNote("some description", "some text", "2022-10-24", "some link", "some mention", "low");
 		ArrayList<Notes> list = notesList.addNoteToList(note);
 		return list;
 	}
+	
+//	@GetMapping("/notes/{id}")
+//	public ArrayList<Notes> retrieveUser(@PathVariable int id){
+//		User user= service.findOne(id);
+//		
+//		if (user==null) {
+//			throw new UserNotFoundException("id:"+id);
+//		}
+//		return list;
+//	}
+	
 }
